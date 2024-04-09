ab5bc6cfe81d9988eec49d2005cc36688adc5421
rogerplis <rogerplis@hotmail.com>
2024-04-09 15:00:04 -0300

    arrumando

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,8 +19,7 @@ export default async function Page() {
             value={numberOfCustomers}
             type="customers"
             />
-             */}
-             
+             */}             
         </div>
         <div className="int-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
              <RevenueChart revenue={revenue}/> 
